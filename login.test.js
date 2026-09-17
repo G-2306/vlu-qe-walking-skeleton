b9c53ae1dd08ec83564e70d0654e277611ff0745
G-2306 <huytruong2306@gmail.com>
2026-09-17 16:52:57 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,17 +1,5 @@
 const { login } = require("./login");
 
-describe("Login Function", () => {
-
-    test("Đăng nhập thành công với admin và 123", () => {
-        expect(login("admin", "123")).toBe(true);
-    });
-
-    test("Đăng nhập thất bại với sai password", () => {
-        expect(login("admin", "456")).toBe(false);
-    });
-
-    test("Đăng nhập thất bại với sai username", () => {
-        expect(login("user", "123")).toBe(false);
-    });
-
+test("Login with correct username and password", () => {
+    expect(login("admin", "123")).toBe(true);
 });
